3d73d4c060f71f573adf5695be36f29a2a56858f
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-07-22 12:47:20 +0200

    feat: implement article creation with repository, actions, and route

--- server/src/modules/article/articleRepository.ts
+++ server/src/modules/article/articleRepository.ts
@@ -13,6 +13,14 @@ class ArticleRepository {
     );
     return rows;
   }
+
+  async createArticle(body: Article) {
+    const [result] = await databaseClient.query(
+      "INSERT INTO article (title, description, image, price, maker_id) VALUES (?, ?, ?, ?, ?)",
+      [body.title, body.description, body.image, body.price, body.maker_id],
+    );
+    return result;
+  }
 }
 
 export default new ArticleRepository();
---
+++ server/src/types/express/article.ts
@@ -0,0 +1,8 @@
+interface Article {
+  id?: number;
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  maker_id: number;
+}
