9ea33ab6757ddea75309bca315c230a0dcfa9156
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-07-24 14:38:36 +0200

    feat: complete article management page

--- client/src/pages/ArticleManagementPage/ArticleManagementPage.tsx
+++ client/src/pages/ArticleManagementPage/ArticleManagementPage.tsx
@@ -1,13 +1,15 @@
+import "./ArticleManagementPage.css";
 import AddArticle from "../../components/AddArticle/AddArticle";
 import ArticleList from "../../components/ArticleList/ArticleList";
 
 function ArticleManagementPage() {
   return (
-    <div>
+    <main className="article-management-page">
       <h1>Gestion des articles</h1>
       <AddArticle />
+      <h2>Modifier ou supprimer un article</h2>
       <ArticleList />
-    </div>
+    </main>
   );
 }
 
